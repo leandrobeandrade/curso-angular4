d9ecb52de9680a44924b25cf075f497fb4dd2eb5
Leandro B Andrade <leandrobe.andrade@gmail.com>
2019-11-15 12:32:13 -0300

    http

--- projeto2/src/app/home/home.component.ts
+++ projeto2/src/app/home/home.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
+  public ofertas: Oferta[];
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    // chamada direta
+    /* this.ofertas = this.ofertasService.getOfertas();
+    console.log(this.ofertas); */
+
+    // chamada com Promisse
+    this.ofertasService.getOfertas()
+    .then((ofertas: Oferta[]) => this.ofertas = ofertas) // resolve
+        //,(param: any) => console.log(param)           // reject ou no catch()
+    .catch((param: any) => console.log(param)
+    )
   }
 
 }
---
+++ projeto2/src/app/ofertas.service.ts
@@ -0,0 +1,74 @@
+import { Oferta } from './shared/oferta.model';
+import { resolve } from 'url';
+
+export class OfertasService {
+
+    public ofertas: Array<Oferta> = [
+        {
+            id: 1,
+            categoria: "restaurante",
+            titulo: "Super Burger",
+            descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
+            anunciante: "Original Burger",
+            valor: 29.90,
+            destaque: true,
+            imagens: [
+                {url: "/assets/ofertas/1/img1.jpg"},
+                {url: "/assets/ofertas/1/img2.jpg"},
+                {url: "/assets/ofertas/1/img3.jpg"},
+                {url: "/assets/ofertas/1/img4.jpg"}
+            ]
+        },
+        {
+            id: 2,
+            categoria: "restaurante",
+            titulo: "Cozinha Mexicana",
+            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
+            anunciante: "Mexicana",
+            valor: 32.90,
+            destaque: true,
+            imagens: [
+                {url: "/assets/ofertas/2/img1.jpg"},
+                {url: "/assets/ofertas/2/img2.jpg"},
+                {url: "/assets/ofertas/2/img3.jpg"},
+                {url: "/assets/ofertas/2/img4.jpg"}
+            ]
+        
+        },
+        {
+            id: 4,
+            categoria: "diversao",
+            titulo: "Estância das águas",
+            descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
+            anunciante: "Estância das águas",
+            valor: 31.90,
+            destaque: true,
+            imagens: [
+                {url: "/assets/ofertas/3/img1.jpg"},
+                {url: "/assets/ofertas/3/img2.jpg"},
+                {url: "/assets/ofertas/3/img3.jpg"},
+                {url: "/assets/ofertas/3/img4.jpg"},
+                {url: "/assets/ofertas/3/img5.jpg"},
+                {url: "/assets/ofertas/3/img6.jpg"}
+            ]
+        }
+    ]
+
+    // chamada direta
+    /* public getOfertas(): Array<Oferta> {
+        return this.ofertas;
+    } */
+
+    // chamada com Promisse
+    public getOfertas(): Promise <Oferta[]> {
+        return new Promise((resolve, reject) => {
+            let retorno = false;  // retorno da api
+
+            if(!retorno) resolve(this.ofertas);
+            else reject({
+                codigo_erro: '404',
+                msg_erro: 'Produtos não encontrados!'
+            })
+        })
+    }
+}
